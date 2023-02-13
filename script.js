852835a2435a5a6a58c1047d222b400919564fd2
nuri <cnr1108@naver.com>
2023-02-13 11:25:05 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function viewTabsOn(){
+        let v = document.getElementById("mytab");
+        if(v.style.display =='none'){
+            v.style.display= 'block';
+        }else{
+            v.style.display='none';
+        }
+}
